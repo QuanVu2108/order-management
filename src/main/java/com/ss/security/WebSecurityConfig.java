653567b5766b1f9c0deb784f73f04d744df02ed6
HungDN <dnhung.it@gmail.com>
2024-04-02 07:27:03 +0000

    fix CORS

--- src/main/java/com/ss/security/WebSecurityConfig.java
+++ src/main/java/com/ss/security/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package com.ss.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,17 +16,53 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${management.endpoints.web.cors.allowed-origins:*}")
+    private List<String> allowedOrigins;
+
+    @Value("${management.endpoints.web.cors.allowed-methods:*}")
+    private List<String> allowedMethods;
+
+    @Value("${management.endpoints.web.cors.allowed-headers:*}")
+    private List<String> allowedHeaders;
+
+    @Value("${management.endpoints.web.cors.allow-credentials:false}")
+    private boolean allowCredentials;
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    @Autowired
+    public FilterRegistrationBean corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+//        source.registerCorsConfiguration("/**", config);
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(allowedMethods);
+        configuration.setAllowedHeaders(allowedHeaders);
+        configuration.setAllowCredentials(allowCredentials);
+        configuration.setMaxAge(Duration.of(86400, ChronoUnit.SECONDS));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.addFilter(corsFilter().getFilter());
 
         // Disable CSRF (cross site request forgery)
         http.csrf().disable();
